f73f2d34c8e62a4d10cd240ec21e52933daeba4f
MYST-Drakonoid <apet2554@gmail.com>
2023-01-11 15:56:59 -0700

    git commit -m finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,50 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        string continueInput = "no";
+        int finished = 0;
+
+
+         Random randomGenerator = new Random();
+         int randomNum = randomGenerator.Next(1, 20);
+        
+
+
+
+        do 
+        {
+
+
+            Console.Write("please guess a number between 1 and 20: ");
+            string userInput = Console.ReadLine();
+            int intUserInput = int.Parse(userInput);
+
+
+            if (intUserInput == randomNum)
+                {
+                    Console.WriteLine("Correct!");
+                    finished = 20;
+                }
+            else if (intUserInput < randomNum)
+                {
+                    Console.WriteLine("too low, guess again.");
+                }
+            else if (intUserInput > randomNum)
+                {
+                    Console.WriteLine("too high, guess again.");
+                }
+            if (finished < 2)
+            {
+                Console.Write("would you like to try again?: ");
+                continueInput = Console.ReadLine();
+                if (continueInput == "no")
+                    {
+                        finished = 20;
+                        Console.WriteLine("Thank you for playing! Game Over");
+                    }
+            }
+
+        } while (finished < 2);
     }
 }
