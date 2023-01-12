684ce3dde8c6e112d42b076fe1ba8be90541d226
MYST-Drakonoid <apet2554@gmail.com>
2023-01-11 21:23:49 -0700

    git commit -m finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,48 @@
-using System;
-
 class Program
-{
+{   
+    
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+                
+        DisplayWelcome();
+
+        string name = GetName();
+
+        int number = GetFavoriteNumber();
+
+        InfoSpitOut(name, number);
+            
+
     }
+    static void DisplayWelcome()
+        {
+            Console.WriteLine("welcome to my program!");
+        }
+
+    static string GetName()
+        {
+            Console.Write("Please enter your name: ");
+            string username = Console.ReadLine();
+            return username;
+            
+        }
+    static int GetFavoriteNumber()
+        {
+            Console.Write("Please enter your Favorite number: ");
+            string number= Console.ReadLine();
+            int intNumber = int.Parse(number);
+            int sQRNumber = intNumber * intNumber;
+            return sQRNumber;
+            
+        }
+
+    static void InfoSpitOut(string name, int number)
+        {
+            Console.WriteLine($"{name} the sqare of your favorite number is {number}");
+        }
+    
+    
 }
+
