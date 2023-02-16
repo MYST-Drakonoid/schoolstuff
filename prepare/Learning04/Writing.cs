6194c586e1487795df43899790db3b5c179e45a6
MYST-Drakonoid <apet2554@gmail.com>
2023-02-16 02:26:43 -0700

    git commit -m finished the code

--- prepare/Learning04/Writing.cs
+++ prepare/Learning04/Writing.cs
@@ -1,19 +1,18 @@
-using System;
-
 public class WritingAssignment : Assignment
-    {
-        private string _title;
 
-        public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
-            {
+{
+    private string _title;
 
-                // setting variables
-                _title = title;
-                
-            }
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+        {
 
-        public string GetWritingInformation()
-            {
-                return $"{_title} by {GetStudentName()}";
-            }
-    }
+            // setting variables
+            _title = title;
+            
+        }
+
+    public string GetWritingInformation()
+        {
+            return $"{_title} by {GetStudentName()}";
+        }
+}
