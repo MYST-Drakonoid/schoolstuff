e591d29c1eaae6a39e57f9961d5535d98393a69b
MYST-Drakonoid <apet2554@gmail.com>
2023-03-29 14:07:30 -0700

    git commmit -m file idetifier complete

--- sandbox/Sandbox/FileIdentifier.cs
+++ sandbox/Sandbox/FileIdentifier.cs
@@ -4,20 +4,64 @@ public class FileTypeIdentifer
 {
     public bool IsFileBinary(string filename)
     {
-        bool answer = true;
-        return answer;
+        Console.WriteLine("checking to see if the file is binary");
+        List<string> f16 = GetFirst16(filename);
+        Console.WriteLine("obtained first 16 characters, processing info...");
+        bool result = IsStringBinary(f16);
+        string entry = "";
+        if (result == true)
+        {
+            entry = "binary";
+        }
+        else
+        {
+            entry = "text";
+        }
+        Console.WriteLine($"the text file is in {entry}");
+        return result;
     }
 
-    public string GetFirst16(string Filename)
+    private List<string> GetFirst16(string Filename)
     {
-        string answer = null;
-        return answer;
+        string[] file = System.IO.File.ReadAllLines(Filename);
+        
+        List<string> f16 = new List<string>();
+        int length = 0;
+        while (length != 16)
+        {
+            foreach (string item in file)
+            {
+                string[] chara = item.Split();
+                foreach (string part in chara)
+                {
+                    if (length != 16)
+                    {
+                        f16.Add(part);
+                        length ++;
+
+                    }
+
+                }
+            }
+        }
+        return f16;
     }
 
-    private bool IsStringBinary(string text)
+    private bool IsStringBinary(List<string> f16)
     {
-        bool answer = true;
+        int truecount = 0;
+        bool answer = false;
+        foreach (string item in f16)
+        {
+            if (item == "1" | item == "0")
+            {
+                truecount ++;
+            }
+        }    
+        if (truecount == 16)
+        {
+            answer = true;
+        }
         return answer;
     }
-
 }
